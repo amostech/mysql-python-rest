227e4fbbafecaa89b60c95128ce60e2403fc1f5e
samarth_mathur <samarth_mathur@filemaker.com>
2018-04-30 00:53:50 -0700

    Change query

--- rest-api/api.py
+++ rest-api/api.py
@@ -15,7 +15,7 @@ mysql.init_app(app)
 @app.route('/')
 def get():
     cur = mysql.connect().cursor()
-    cur.execute('''select UNIQUE_CARRIER, count(*) from airlinedatadb.flights where ARR_DELAY > 30 group by UNIQUE_CARRIER order by count(*) desc;''')
+    cur.execute('''select * from airlinedatadb.flights''')
     r = [dict((cur.description[i][0], value)
               for i, value in enumerate(row)) for row in cur.fetchall()]
     return jsonify({'carrier_delays' : r})
